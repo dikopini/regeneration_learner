5b4a881af0babe84cdde72be46afb8cb08b6d6ea
irfan_basyar <irfanbasyar.ib@gmail.com>
2023-04-04 21:42:08 +0700

    bismillah

--- main.py
+++ main.py
@@ -5,3 +5,4 @@ ini adalah program belajar fase kedua sampai menembus remote work
 print('Hello world!')
 print('Aku Pasti Bisa!')
 print('sukses selalu!')
+print('istiqomah, sabar, dan iringi dengan doa')
