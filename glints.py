7afe5b4ca8ce4097c4d96c9a9315be21caaea41b
irfan_basyar <irfanbasyar.ib@gmail.com>
2023-04-06 18:44:31 +0700

    final get contents

--- glints.py
+++ glints.py
@@ -41,27 +41,31 @@ def get_total_page():
 
 
 def get_content():
+    job_list = []
     contents = soup.find('div', 'm-b-40')
     # loker
     # perusahaan
     # pendidikan
     # lokasi
-    """
+
     all_loker = contents.find_all('h3', 'media-heading h4')
 
     for i in all_loker:
         loker = i.find('a').text
-        print(loker)
-    """
+        dict1 = {
+            'lowongan kerja': loker
+        }
 
     table = contents.find_all('table', 'table')
     for i in table:
+        loker = i.find('a').text
+        print(loker)
         items = i.findAll('tr')
 
         perusahaan = items[0]
         perusahaan = perusahaan.findAll('td')
         perusahaan = str(perusahaan[1])
-        perusahaan = perusahaan.replace('<td>','').replace('</td>','')
+        perusahaan = perusahaan.replace('<td>', '').replace('</td>', '')
         print(perusahaan)
 
         pendidikan = items[1]
@@ -76,10 +80,50 @@ def get_content():
         lokasi = lokasi.replace('<td>', '').replace('</td>', '')
         print(lokasi)
 
-        print()
-        # for i in perusahaan:
-        #    print(i)
+        dict2 = {
+            'perusahaan': perusahaan,
+            'pendidikan': pendidikan,
+            'lokasi': lokasi
+        }
+
+
+def percobaan():
+    contents = soup.find('div', 'm-b-40')
+    all_loker = contents.find_all('h3', 'media-heading h4')
+    a = 0
+
+    table = contents.find_all('table', 'table')
+    job_list = []
+    for i in table:
+
+        job = all_loker[a].find('a').text
+        a = a+1
+
+        items = i.findAll('tr')
+
+        perusahaan = items[0]
+        perusahaan = perusahaan.findAll('td')
+        perusahaan = str(perusahaan[1])
+        perusahaan = perusahaan.replace('<td>', '').replace('</td>', '')
+
+        pendidikan = items[1]
+        pendidikan = pendidikan.findAll('td')
+        pendidikan = str(pendidikan[1])
+        pendidikan = pendidikan.replace('<td>', '').replace('</td>', '')
+
+        lokasi = items[2]
+        lokasi = lokasi.findAll('td')
+        lokasi = str(lokasi[1])
+        lokasi = lokasi.replace('<td>', '').replace('</td>', '')
+
+        data_dict = {
+            'lowongan kerja': job,
+            'perusahaan': perusahaan,
+            'pendidikan': pendidikan,
+            'lokasi': lokasi
+        }
+        job_list.append(data_dict)
 
 
 if __name__ == '__main__':
-    get_content()
+    percobaan()
